eeff3bbe7ba56c632b84cacfffa0254d0697878d
Supanut Apikulvanich <SugarSeeker@Supanuts-MacBook-Air.local>
2015-01-29 17:53:26 +0700

    update render

---
+++ gg.cs
@@ -0,0 +1,6 @@
+using System;
+class test {
+	static void Main() {
+		decimal a = 2;
+	}
+}
---
+++ vvv.cs
@@ -0,0 +1,11 @@
+using System;
+class quiz03 {
+	    static void Main() {
+			        Console.Write("Input X : ");
+					        int x = int.Parse(Console.ReadLine());
+							        double a = 1; 
+										        Console.WriteLine("Output A : {0:f3}", a);
+									        Console.ReadLine();
+											    }
+
+}
